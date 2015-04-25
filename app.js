ff26cde6b99d7aa736edbb2fe8b8fbaf4db7ff3f
sequinchapman <matthewjamesbird@yahoo.com.au>
2015-04-25 23:29:28 +1000

    add express

--- app.js
+++ app.js
@@ -1,60 +1,24 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var http = require("http");
+/*http.createServer(function(request, response){
+    response.writeHead(200);
+    request.pipe(response);
+}).listen(8080);
+console.log(("listening on port 8080..."));*/
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var fs = require('fs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+http.createServer(function(request, response){
+    var newFile = fs.createWriteStream("readme_copy.md");
+    var fileBytes = request.headers['content-length'];
+    var uploadedBytes = 0;
+    request.pipe(newFile);
+    request.on('data',function(chunk){
+        uploadedBytes += chunk.length;
+        var progress = (uploadedBytes / fileBytes) * 100;
+        response.write("progress: " +  parseInt(progress, 10) + "%\n");
     });
-  });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+}).listen(8080);
+console.log(("listening on port 8080..."));
